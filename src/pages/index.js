12c7fe9cc494c9473b64516ed6f80348cc3b3345
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2020-07-31 16:37:30 +0200

    added a date

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => (
     <header className="masthead">
       <div className="container h-100">
         <div className="row h-100">
-          <div className="col-lg-7 my-auto">
+          <div className="col-lg-5 my-auto">
             <div className="header-content mx-auto">
               <h1 className="mb-5">
                 Nutritional Technology
@@ -34,9 +34,10 @@ const IndexPage = () => (
                 </a>
             </div>
           </div>
-          {/**<div className="col-lg-5 my-auto">
-                    <img src={demo1} className="img-fluid" alt="" />
-                  </div>*/}
+          <div className="col-lg-7 my-auto">
+          <h1 className="section-heading">
+            1 September - 13 October 2020
+          </h1>                  </div>
                 </div>
               </div>
     </header>
